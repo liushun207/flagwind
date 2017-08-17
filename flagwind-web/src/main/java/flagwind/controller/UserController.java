3823d2eccc3b51835d0d82be1a2362225fb3f609
刘顺 <642517491@qq.com>
2017-08-17 22:25:20 +0800

    格式化代码

--- flagwind-web/src/main/java/flagwind/controller/UserController.java
+++ flagwind-web/src/main/java/flagwind/controller/UserController.java
@@ -2,12 +2,14 @@ package flagwind.controller;
 
 import flagwind.pojo.User;
 import flagwind.service.IUserService;
+import org.joda.time.DateTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
 /**
  * The type User controller.
@@ -19,7 +21,7 @@ public class UserController
     @Resource
     private IUserService userService;
 
-    @RequestMapping(value = "/show",method = RequestMethod.GET)
+    @RequestMapping(value = "/show", method = RequestMethod.GET)
     @ResponseBody
     public User helloMvc(long userId)
     {
@@ -27,7 +29,7 @@ public class UserController
         return user;
     }
 
-    @RequestMapping(value = "/get",method = RequestMethod.GET)
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
     public User getUser(String name, String password)
     {
@@ -35,7 +37,23 @@ public class UserController
         return user;
     }
 
-    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @ResponseBody
+    public User login(String name, String password)
+    {
+        User user = this.userService.getUser(name, password);
+
+        if (user != null)
+        {
+            user.setLastip("127.0.0.1");
+            user.setLastvisit(new Date());
+            this.userService.loginSucess(user);
+        }
+
+        return user;
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
     @ResponseBody
     public String test() throws Exception
     {
